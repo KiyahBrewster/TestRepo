2bdea579b3571c1daedf2418857dd8443ea0ad1a
KiyahBrewster <kiyah81@gmail.com>
2025-06-05 13:48:52 -0500

    Completed

---
+++ JAVADUMP-EXERCISES/Strings_Shopping_Cart/ShoppingCartApp.java
@@ -0,0 +1,18 @@
+public class ShoppingCartApp {
+    public static void main(String[] args) {
+
+        // String details for the business
+        String businessName = "Tech Haven Electronics";
+        String contactInfo = "Contact: support@techhaven.com | (123) 456-7890";
+
+        // Item description
+        String itemDescription = "Wireless Bluetooth Headphones with noise cancellation.";
+
+        // Print to console
+        System.out.println("Welcome to " + businessName + "!");
+        System.out.println(contactInfo);
+        System.out.println("----------------------------------");
+        System.out.println("Item Description: " + itemDescription);
+        System.out.println("Thank you for shopping with us!");
+    }
+}
