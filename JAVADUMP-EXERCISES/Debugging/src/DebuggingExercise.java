2038261579bdf7d1b4874e3d350d8b2c4a6e32d7
KiyahBrewster <kiyah81@gmail.com>
2025-06-09 16:32:16 -0500

    Completed

---
+++ JAVADUMP-EXERCISES/Debugging/src/DebuggingExercise.java
@@ -0,0 +1,36 @@
+public class DebuggingExercise {
+
+    public static void main(String[] args) {
+        System.out.println("Starting Debugging Exercise...");
+
+        int[] numbers = {5, 10, 15, 20, 25};
+
+        System.out.println("Calculating Sum...");
+        int total = calculateSum(numbers);
+        System.out.println("Sum: " + total);
+
+        System.out.println("Calculating Factorial...");
+        int factorialResult = factorial(5);
+        System.out.println("Factorial of 5: " + factorialResult);
+
+        System.out.println("Debugging Exercise Complete!");
+    }
+
+    // Fixed method to calculate sum of an array
+    public static int calculateSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) { // Fix: Start from 0, use < instead of <=
+            sum += nums[i]; // Correct index usage
+        }
+        return sum;
+    }
+
+    // Recursive method to calculate factorial
+    public static int factorial(int num) {
+        if (num == 0) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+}
